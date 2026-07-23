91de200e140f26e9899be204c3ce3328c3a1be3b
FerDPP <ferdypputra17@gmail.com>
2026-07-23 11:02:04 +0700

    Fix api import in Settings.jsx

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import api from '../api/api';
+import { api } from '../api/api';
 import { ICONS } from './Icons';
 import { useAuth } from '../context/AuthContext';
 
